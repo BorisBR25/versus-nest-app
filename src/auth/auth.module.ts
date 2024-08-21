8c8cca15cc9501b89ee2c5bcf586e97b860a0a56
BorisBR25 <jbarriosricardo@gmail.com>
2024-08-21 01:36:24 -0500

    Auth module added with jwt

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { env } from 'process';
+
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
--- src/auth/dto/signUp.dto.ts
+++ src/auth/dto/signUp.dto.ts
@@ -1,19 +1,23 @@
-import { Transform } from 'class-transformer';
-import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { Transform } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class CreateUserDto {
+export class SignUpDto {
 
     @Transform(({ value }) => value.trim())
     @IsString()
     @IsNotEmpty()
     name: string;
-    
+
     @IsEmail()
     email: string;
     
     @Transform(({ value }) => value.trim())
     @IsString()
+    @MinLength(6)
     @IsNotEmpty()
-    @MinLength(8)
     password: string;
+    
+    
+    
+    
 }
